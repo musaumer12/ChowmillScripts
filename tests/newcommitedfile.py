b204572eb71e00977fa8d88cee0168017780ab6d
musa umer <musaumer8@gmail.com>
2024-08-02 15:53:56 +0500

    second commit

---
+++ tests/newcommitedfile.py
@@ -0,0 +1,22 @@
+import time
+
+# chrome driver
+# -- Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
+import pytest
+
+from PageObjects.Logout import Logout
+from PageObjects.YourOrders import YourOrders
+from PageObjects.SignInPage import SignIn
+from TestData.SigninData import SigninData
+from utilities.BaseClass import BaseClass
+
+
+driver = webdriver.Chrome()
+driver.get("https://app-dev.chowmill.com/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.close()
